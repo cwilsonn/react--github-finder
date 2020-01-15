722d9d6b364a56d7d2a738d86ef7109cc2d07655
Cody Wilson <cody@tangentaz.com>
2020-01-15 00:15:32 -0700

    Adjust height on UserItem to be 100% & fill parent .col-*

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-3">
-      <div className="card border-0 shadow-sm p-4">
+      <div className="card h-100 border-0 shadow-sm p-4">
         <img
           className="rounded-circle card-img-top h-50 w-50 mx-auto mb-2"
           src={avatar_url}
